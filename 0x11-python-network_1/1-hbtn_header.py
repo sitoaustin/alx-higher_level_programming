aa52cc0d54b96c585011e488dd38a3427defdb86
Xys-Tus <Sixtus Onyedibe>
2023-03-31 18:30:33 +0100

    response header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" a Python script that takes in a URL,
+sends a request to the URL and displays the value of the 
+X-Request-Id variable found in the header of the response.
+"""
+
+if __name__ == "__main__":
+    import urllib.request
+    user_input = input("Enter username:")
+    req = urllib.request.Request('{}'.format(user_input))
+    with urllib.request.urlopen(req) as response:
+        content = response.header
+        print(content)
