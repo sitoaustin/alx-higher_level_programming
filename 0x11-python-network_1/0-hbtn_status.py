0b300c2f4b30658fd2d4171a484191ff16e150fb
Xys-Tus <Sixtus Onyedibe>
2023-03-31 18:13:27 +0100

    updated my script

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""A script that fetches 
+https://alx-intranet.hbtn.io/status
+and uses urllib library
+"""
 
 import urllib.request
 
@@ -6,6 +10,8 @@ if __name__ == "__main__":
     the_page = ""
     req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
-        print(response.read())
-        print(response.read())
-        print(response.read())
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
