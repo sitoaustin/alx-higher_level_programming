a1e9d57af1235d4908213f861b99a99e9b2ed918
Xys-Tus <Sixtus Onyedibe>
2023-04-01 19:56:52 +0100

    Http error handling

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,10 +11,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    try: 
+    try:
         req = urllib.request.Request(url)
         with urllib.request.urlopen(req) as response:
             print(response.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
-        
