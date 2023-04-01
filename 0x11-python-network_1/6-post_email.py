77eab4cde0b28b4e1904f9e25d1f575341d12291
Xys-Tus <Sixtus Onyedibe>
2023-04-01 21:56:18 +0100

    post email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,8 +7,7 @@ as a parameter, and displays the body of the response
 import requests
 import sys
 if __name__ == "__main__":
-    
     url = sys.argv[1]
-    payload = {"email": sys.argv[2]}
+    payload = {'email': sys.argv[2]}
     response = requests.post(url, data=payload)
-    print(response.content)
+    print(response.text)
