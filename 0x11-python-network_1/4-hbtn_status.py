f00f423646cef61482793c30b08a4c0231857e12
Xys-Tus <Sixtus Onyedibe>
2023-04-01 21:08:28 +0100

    python requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,7 +8,7 @@ import requests
 if __name__ == "__main__":
     response = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(response)))
-    print("\t- content: {}".format(response))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.content))
     print(response)
     # print("\t- utf8 content: {}".format(response.decode('utf-8')))
