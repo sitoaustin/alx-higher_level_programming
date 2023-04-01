0208d4e29b881a00bd9c08e10805c149b67d2d00
Xys-Tus <Sixtus Onyedibe>
2023-04-01 23:56:19 +0100

    list 10 commits on github

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" a Python script  that  gets
+commits
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    response = requests.get('https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2], sys.argv[1]))
+    print(response)
