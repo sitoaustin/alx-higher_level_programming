987cde48d2bf1664ec738d32b7cdb978896f3fd7
Xys-Tus <Sixtus Onyedibe>
2023-04-01 18:56:49 +0100

    feat

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,9 +9,26 @@ if __name__ == "__main__":
     import urllib.request
     import sys
     url = sys.argv[1]
-    value = sys.argv[2]
+    value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value)
     data = data.encode('ascii') # data should be bytes
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
+
+
+
+"""
+if __name__ == "__main__":
+    import sys
+    from urllib.parse import urlencode
+    from urllib.request import Request, urlopen
+    url = sys.argv[1]
+    data = sys.argv[2]
+    req = Request(url, urlencode(data).encode())
+    with Request(req) as response:
+        content = response.read().decode('utf-8')
+        print(content)
+    # response = urlopen(req).read().decode()
+    # print(response)
+"""
