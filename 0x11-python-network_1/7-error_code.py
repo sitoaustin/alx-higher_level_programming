8639c0376357f0a58ad659b8702f1889e96f16d9
Xys-Tus <Sixtus Onyedibe>
2023-04-01 22:06:51 +0100

    error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" a Python script  that takes in a URL and an email,
+sends a POST request to the passed URL with the email
+as a parameter, and displays the body of the response
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response))
+    else:
+        print(response.text)
