0303d4e6b113bf63849c7123dc93d373d246eab0
Xys-Tus <Sixtus Onyedibe>
2023-04-01 21:51:30 +0100

    post email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     payload = {"email": sys.argv[2]}
     response = requests.post(url, data=payload)
-    print(response)
+    print(response.content)
