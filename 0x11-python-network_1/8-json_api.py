46180f4bcd06a7f4deabe16126dbe7292f1846c2
Xys-Tus <Sixtus Onyedibe>
2023-04-01 22:32:41 +0100

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-""" a Python script  that  takes in a 
-letter and sends a POST request to 
-http://0.0.0.0:5000/search_user with 
+""" a Python script  that  takes in a
+letter and sends a POST request to
+http://0.0.0.0:5000/search_user with
 the letter as a parameter.
 """
 import requests
 import sys
 if __name__ == "__main__":
-    if not sys.argv[1]:
-        input_payload = ""
-    else:
+    if len(sys.argv) > 1:
         input_payload = sys.argv[1]
+    else:
+        input_payload = ""
     payload = {'q': input_payload}
     response = requests.post("http://0.0.0.0:5000/search_user", data=payload)
-    print(response.text)
+    try:
+        response.raise_for_status()
+        json = response.json()
+        if len(json) == 0:
+            print("No result")
+        else:
+            print("[{:d}] {}".format(json['id'], json['name']))
+    except Exception:
+        print("Not a valid JSON")
