bac7e78808e2046e1a44138a49d0efc05ea0c715
Xys-Tus <Sixtus Onyedibe>
2023-04-01 22:14:42 +0100

    error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
     if response.status_code >= 400:
-        print("Error code: {}".format(response))
+        print("Error code: {}".format(response.status_code))
     else:
-        print(response.status_code)
+        print(response.text)
