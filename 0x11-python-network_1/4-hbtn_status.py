386a64a066878927ef7e7cfb00c5d7db08c9443f
Xys-Tus <Sixtus Onyedibe>
2023-04-01 21:10:22 +0100

    python requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,5 +10,3 @@ if __name__ == "__main__":
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.content))
-    print(response)
-    # print("\t- utf8 content: {}".format(response.decode('utf-8')))
