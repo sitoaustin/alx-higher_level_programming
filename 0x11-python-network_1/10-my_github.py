a47e320d02fcb4096751bbab8ba7ea85f882482d
Xys-Tus <Sixtus Onyedibe>
2023-04-01 23:08:18 +0100

    script that takes github creds and display your id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,6 +8,7 @@ import requests
 if __name__ == "__main__":
     user_name = sys.argv[1]
     pass_w = sys.argv[2]
-    params = {"username": user_name, "auth": pass_w}
-    res = requests.get("users/{}".format(user_name), data=params)
+    # payload = {"username": user_name, "auth": pass_w}
+    headers = {'Authorization': f'token {pass_w}'}
+    res = requests.get("https://api.github.com/users/{}/".format(user_name), headers=headers)
     print(res)
